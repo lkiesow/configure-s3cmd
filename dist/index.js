c0f9182e6b9399dd528cef7e53a40e6d16527af7
Lars Kiesow <lkiesow@uos.de>
2026-08-08 15:52:27 +0200

    Consolidate build tsconfig with the ES2022/NodeNext base config
    tsconfig.json (used by npm run build) still targeted ES6/CommonJS by
    hand, disconnected from tsconfig.base.json/tsconfig.eslint.json which
    already assumed ES2022. Point the build config at the shared base
    (keeping CommonJS output for now, ahead of the ESM migration) and drop
    the dangling __fixtures__/__tests__/jest.config.js/rollup.config.ts
    references in tsconfig.eslint.json, none of which exist in this repo.

--- dist/index.js
+++ dist/index.js
@@ -39,26 +39,16 @@ var __importStar = (this && this.__importStar) || (function () {
         return result;
     };
 })();
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(7484));
 const fs = __importStar(__nccwpck_require__(9896));
 const os = __importStar(__nccwpck_require__(857));
-function run() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const host = core.getInput('host');
-            const access_key = core.getInput('access_key');
-            const secret_key = core.getInput('secret_key');
-            const config = `
+async function run() {
+    try {
+        const host = core.getInput('host');
+        const access_key = core.getInput('access_key');
+        const secret_key = core.getInput('secret_key');
+        const config = `
 	 	host_base = ${host}
 		host_bucket = ${host}
 		bucket_location = us-east-1
@@ -67,15 +57,14 @@ function run() {
 		secret_key = ${secret_key}
 		signature_v2 = False
 	 `.replace(/\n[\t ]+/g, '\n');
-            const home = os.homedir();
-            fs.writeFileSync(`${home}/.s3cfg`, config);
+        const home = os.homedir();
+        fs.writeFileSync(`${home}/.s3cfg`, config);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message);
         }
-        catch (error) {
-            if (error instanceof Error) {
-                core.setFailed(error.message);
-            }
-        }
-    });
+    }
 }
 run();
 
